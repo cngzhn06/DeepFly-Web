909ddf2b6d7455caaa1d674684f1fc6bd7438f64
cengizhan <cngzhnc06@gmail.com>
2024-10-30 22:32:23 +0300

    deploy

--- src/components/Content/Features.tsx
+++ src/components/Content/Features.tsx
@@ -10,6 +10,45 @@ import {
 } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 
+const featureItems = [
+  {
+    icon: <BrainCircuitIcon className="mx-auto" size={32} />,
+    title: "Çeşitli Mantık Problemlerine Erişim",
+    description:
+      "Uygulamanızda çocuklar farklı mantık problemlerine erişebilir. Örneğin, \"farklı olanı bul\" veya \"sudoku\" gibi oyunlar sayesinde çocuklar algoritmalarla bir yol bularak ilerleyebilirler. Bu sayede çocuklar, çeşitli kaynaklardan mantık problemlerine ulaşarak yaratıcı çözümler geliştirmeyi öğrenirler.",
+  },
+  {
+    icon: <MousePointer2 className="mx-auto" size={32} />,
+    title: "Problemleri Takip Etme ve İlerleme Kayıtları",
+    description:
+      "Çocuklar çözüm aşamalarında ilerleme kaydeder ve puanlarını uygulama içerisinde izleyebilirler. Bu özellik, farklı seviyelerdeki oyunların bir bütün olarak çocukların gelişim sürecini takip etmelerini sağlar.",
+  },
+  {
+    icon: <Settings2Icon className="mx-auto" size={32} />,
+    title: "Kişiselleştirilebilir Zorluk Seviyeleri",
+    description:
+      "Oyunları çocukların seviyesine göre özelleştirmek, farklı zorluk seviyelerindeki görevlerin yaratılmasını sağlar. Çocuklar, kendi seviyelerine uygun problemlerle uğraşırken, başarı ve motivasyonları artar.",
+  },
+  {
+    icon: <Clock10Icon className="mx-auto" size={32} />,
+    title: "Günlük Zeka Soruları ve Zamanlayıcı",
+    description:
+      "Çocukların her gün yeni bir zeka sorusuna cevap verme şansı tanınabilir. Örneğin, \"Bugünün Zeka Sorusu\" gibi bir bölümle çocuklar, belirli bir süre içinde problemi çözmek için çalışarak, günlük bir rutin oluşturabilirler.",
+  },
+  {
+    icon: <PhoneCallIcon className="mx-auto" size={32} />,
+    title: "İletişim ve Sosyal Paylaşım Alanı",
+    description:
+      "Çocuklar başarılarını ve çözümlerini arkadaşlarıyla paylaşabilir veya diğer kullanıcılarla ipuçları üzerinde tartışabilirler. Bu sayede çocuklar takım çalışması yapmayı ve farklı bakış açılarını öğrenirler.",
+  },
+  {
+    icon: <BarChart2Icon className="mx-auto" size={32} />,
+    title: "Gelişim ve İlerlemenin Analizi",
+    description:
+      "Uygulama, çocukların gelişimini ve beceri kazanımlarını analiz edebilir. Örneğin, çocuklar hangi zeka türlerinde daha güçlü olduklarını görebilir, böylece odaklanmaları gereken alanları belirleyebilirler.",
+  },
+];
+
 export const Features = () => {
   const [hasAnimated, setHasAnimated] = useState(false); // Track if animation has already triggered
   const sectionRef = useRef<HTMLDivElement>(null); // Reference to the section
@@ -31,110 +70,24 @@ export const Features = () => {
       className="py-32 font-poppins"
     >
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-
-
         <div className="grid grid-cols-1 gap-2 mt-10 text-center sm:mt-16 sm:grid-cols-2 sm:gap-x-12 gap-y-12 md:grid-cols-3 md:gap-4 xl:mt-24">
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 0.7 }}
-            className="p-12 card bg-primary-content"
-          >
-            <BrainCircuitIcon className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-              Çeşitli Mantık Problemlerine Erişim
-            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-              Uygulamanızda çocuklar farklı mantık problemlerine erişebilir.
-              Örneğin, "farklı olanı bul" veya "sudoku" gibi oyunlar sayesinde
-              çocuklar algoritmalarla bir yol bularak ilerleyebilirler. Bu
-              sayede çocuklar, çeşitli kaynaklardan mantık problemlerine
-              ulaşarak yaratıcı çözümler geliştirmeyi öğrenirler.
-            </p>
-          </motion.div>
-
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 0.8 }}
-            className="p-12 card bg-primary-content"
-          >
-            <MousePointer2 className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-            Problemleri Takip Etme ve İlerleme Kayıtları            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-              Çocuklar çözüm aşamalarında ilerleme kaydeder ve puanlarını
-              uygulama içerisinde izleyebilirler. Bu özellik, farklı
-              seviyelerdeki oyunların bir bütün olarak çocukların gelişim
-              sürecini takip etmelerini sağlar.
-            </p>
-          </motion.div>
-
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 0.9 }}
-            className="p-12 card bg-primary-content"
-          >
-            <Settings2Icon className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-            Kişiselleştirilebilir Zorluk Seviyeleri
-            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-            Oyunları çocukların seviyesine göre özelleştirmek, farklı zorluk seviyelerindeki görevlerin yaratılmasını sağlar. Çocuklar, kendi seviyelerine uygun problemlerle uğraşırken, başarı ve motivasyonları artar.
-            </p>
-          </motion.div>
-
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 1 }}
-            className="p-12 card bg-primary-content"
-          >
-            <Clock10Icon className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-            Günlük Zeka Soruları ve Zamanlayıcı
-            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-            Çocukların her gün yeni bir zeka sorusuna cevap verme şansı tanınabilir. Örneğin, "Bugünün Zeka Sorusu" gibi bir bölümle çocuklar, belirli bir süre içinde problemi çözmek için çalışarak, günlük bir rutin oluşturabilirler.
-            </p>
-          </motion.div>
-
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 1.1 }}
-            className="p-12 card bg-primary-content"
-          >
-            <PhoneCallIcon className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-            İletişim ve Sosyal Paylaşım Alanı
-            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-            Çocuklar başarılarını ve çözümlerini arkadaşlarıyla paylaşabilir veya diğer kullanıcılarla ipuçları üzerinde tartışabilirler. Bu sayede çocuklar takım çalışması yapmayı ve farklı bakış açılarını öğrenirler.
-            </p>
-          </motion.div>
-
-          <motion.div
-            ref={sectionRef}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 1.2 }}
-            className="p-12 card bg-primary-content"
-          >
-            <BarChart2Icon className="mx-auto" size={32} />
-            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
-            Gelişim ve İlerlemenin Analizi
-            </h3>
-            <p className="mt-5 text-base text-gray-600 font-pj">
-            Uygulama, çocukların gelişimini ve beceri kazanımlarını analiz edebilir. Örneğin, çocuklar hangi zeka türlerinde daha güçlü olduklarını görebilir, böylece odaklanmaları gereken alanları belirleyebilirler.
-            </p>
-          </motion.div>
+          {featureItems.map((feature, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={hasAnimated ? { opacity: 1, scale: 1 } : {}}
+              transition={{ duration: 0.5 + index * 0.1 }}
+              className="p-12 card bg-primary-content"
+            >
+              {feature.icon}
+              <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">
+                {feature.title}
+              </h3>
+              <p className="mt-5 text-base text-gray-600 font-pj">
+                {feature.description}
+              </p>
+            </motion.div>
+          ))}
         </div>
       </div>
     </motion.section>
--- src/components/Content/HowItWork.tsx
+++ src/components/Content/HowItWork.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export const HowItWork = () => {
   return (
@@ -6,7 +7,7 @@ export const HowItWork = () => {
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto text-center">
           <h2 className="font-poppins text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-            Oyun Nasıl Çalışır?
+            Oyun Nasıl Çalışır
           </h2>
           <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">
             Çocuklar için eğlenceli ve düşündürücü zeka oyunlarıyla dolu bu
@@ -16,17 +17,19 @@ export const HowItWork = () => {
 
         <div className="relative mt-12 lg:mt-20">
           <div className="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28">
-            <img
+            <Image
               className="w-full"
               src="https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg"
-              alt=""
+              alt="Curved Dotted Line"
+              width={1000}  // Gerekli genişlik
+              height={200}  // Gerekli yükseklik
             />
           </div>
 
           <div className="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12">
             <div>
               <div className="animate-pulse flex items-center justify-center w-16 h-16 mx-auto bg-black border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-white"> 1 </span>
+                <span className="text-xl font-semibold text-white">1</span>
               </div>
               <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
                 Oynamak İstediğin Oyuna Tıkla
@@ -35,7 +38,7 @@ export const HowItWork = () => {
 
             <div>
               <div className="animate-pulse flex items-center justify-center w-16 h-16 mx-auto bg-black border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-white"> 2 </span>
+                <span className="text-xl font-semibold text-white">2</span>
               </div>
               <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
                 Görevleri Tamamla
@@ -44,12 +47,13 @@ export const HowItWork = () => {
 
             <div>
               <div className="animate-pulse flex items-center justify-center w-16 h-16 mx-auto bg-black border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-white"> 3 </span>
+                <span className="text-xl font-semibold text-white">3</span>
               </div>
               <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
                 Gizli Özelliklerini Öğren
               </h3>
               <p className="mt-4 text-base text-gray-600">
+                Bu oyunda, her tamamladığın görev sana yeni yetenekler kazandıracak!
               </p>
             </div>
           </div>
